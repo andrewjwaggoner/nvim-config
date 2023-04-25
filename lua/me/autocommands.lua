aaef8bfc176fa67b57f5e077421530fe1d9b6ac9
Andrew Waggoner <me@andrewjwaggoner.com>
2023-04-25 19:28:10 -0400

    add: Redir autocommand

--- lua/me/autocommands.lua
+++ lua/me/autocommands.lua
@@ -59,3 +59,11 @@ end, {
   nargs = 0,
   desc = 'Kill all jupyter notebooks',
 })
+
+vim.api.nvim_create_user_command('Redir', function(ctx)
+  local lines = vim.split(vim.api.nvim_exec(ctx.args, true), '\n', { plain = true })
+  vim.cmd('new')
+  table.remove(lines,1)  -- remove the command from the output
+  vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
+  vim.opt_local.modified = false
+end, { nargs = '+', complete = 'command' })
