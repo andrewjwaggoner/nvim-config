e43d3b671658583fb3a7d83eb2c4b1e152177523
Andrew Waggoner <andrew.waggoner@shellpointmtg.com>
2023-10-25 21:13:44 -0400

    tweaks: cleanup lazy loading work

--- lua/config/markdown_preview.lua
+++ lua/config/markdown_preview.lua
@@ -4,19 +4,12 @@ function markdown_preview.config()
   vim.fn['mkdp#util#install']()
 end
 
-function markdown_preview.cond()
-  local_buffer = vim.fn.bufname("%")
-  match = local_buffer:match('%.md$')
-  print(vim.fn.bufname("%"):match('%.md$') == '.md')
-
-  return vim.fn.bufname("%"):match('%.md$') == '.md'
-end
-
 function markdown_preview.lazy()
   return  {
     'iamcco/markdown-preview.nvim',
     markdown_preview.package_name,
-    cond = markdown_preview.cond,
+    lazy = false,
+    cmd = 'MarkdownPreview',
     config = markdown_preview.config }
 end
 
