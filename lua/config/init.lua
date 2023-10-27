c9f132a22d84ab21e1108d8c5887ad2eb830ad1c
Andrew Waggoner <andrew.waggoner@shellpointmtg.com>
2023-10-27 16:57:17 -0400

    fix: windows command usage

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -4,47 +4,36 @@ local function is_windows()
   return package.config:sub(1, 1) == '\\'
 end
 
-local function get_api_directory_call()
+local function get_directory_command()
   if is_windows() then
-    return "dir /B "
+    return "where " .. vim.fn.getcwd() .. ':*.lua'
   end
-  return "ls " 
+  return "ls "  .. vim.fn.getcwd()
 end
 
-local function get_current_file_path()
-  local str = debug.getinfo(2, "S").source:sub(2)
-  local match = str:match("(.*[/\\])")
-  if is_windows() then
-    match = string.gsub(match, "/", "\\")
-  end
-  return match
-end
-
-local function getLuaFiles(nvim_directory)
-  local luaFiles = {}
-  local cmd = get_api_directory_call() .. nvim_directory .. '*.lua'
-  local success, pipe = pcall(io.popen, cmd, 'r')
+local function get_config_files()
+  local configFiles = {}
+  local success, pipe = pcall(io.popen, get_directory_command(), 'r')
 
   if success then
     for line in pipe:lines() do
-      if line ~= 'init.lua' and line ~= nil then
-        table.insert(luaFiles, line)
+      if line ~= 'init.lua' and line ~= nil then -- ignore current file for obvious reasons
+        table.insert(configFiles, line)
       end
     end
     pipe:close()
   else
+    print('Error while reading config directory: ' .. vim.fn.getcwd())
   end
-  return luaFiles
+  return configFiles
 end
 
 -- Load modules based on file names
-local luaFiles = getLuaFiles(get_current_file_path())
-
-for _, filename in ipairs(luaFiles) do
-    local moduleName = filename:match(".*(config[/\\].+)%..+")
-    if moduleName and moduleName ~= 'config/init' then
-        plugins[moduleName] = require(moduleName)
-    end
+for _, filename in ipairs(get_config_files()) do
+  local moduleName = filename:match(".*[/\\](.*).lua") or ''
+  if moduleName and (moduleName ~= 'init' and moduleName ~= 'config/init') then
+    plugins[moduleName] = require('config/' .. moduleName)
+  end
 end
 
 return plugins
