69939ec6005d4ee0817e2cab59904eccdbac4174
Andrew Waggoner <me@andrewjwaggoner.com>
2023-11-07 21:12:35 -0500

    add: add nvim-dap to config

---
+++ lua/config/nvim_dap.lua
@@ -0,0 +1,22 @@
+local nvim_dap = {}
+
+nvim_dap.config = function()
+  dap = require('dap')
+  print('test config')
+  vim.keymap.set('n', '<Leader>dt', function() dap.toggle_breakpoint() end, { silent = true })
+  vim.keymap.set('n', '<Leader>ds', function() dap.step_over() end, { silent = true })
+  vim.keymap.set('n', '<Leader>di', function() dap.step_into() end, { silent = true })
+  vim.keymap.set('n', '<Leader>dr', function() dap.repl.open() end, { silent = true })
+end
+
+nvim_dap.lazy = function()
+  return {
+    'https://github.com/mfussenegger/nvim-dap',
+    name = 'nvim-dap',
+    lazy = true,
+    config = nvim_dap.config,
+    ft = { "python", "lua", "c", "cpp", "java", "c#" }
+  }
+end
+
+return nvim_dap
