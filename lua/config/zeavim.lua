1b3e001e818d14f9cd4d58e99229884a77e18c95
Andrew Waggoner <me@andrewjwaggoner.com>
2023-12-30 14:59:25 -0500

    update: zeavim

--- lua/config/zeavim.lua
+++ lua/config/zeavim.lua
@@ -1,13 +1,24 @@
 local zeavim = { package_name = 'KabbAmine/zeavim.vim' }
 vim.keymap.set('n', '<Leader>z', '<Cmd>Zeavim<CR>', { silent = true })
+vim.keymap.set('n', '<Leader><Leader>z', '<Plug>ZVKeyDocset', { silent = true })
+vim.keymap.set('n', 'gz', '<Plug>ZVOperator', { silent = true })
+vim.keymap.set('v', '<Leader>z', '<Plug>ZVVisSelection', { silent = true })
 
 function zeavim.config()
   vim.g.zv_file_types = {
-    ['css'] = 'css,foundation',
     ['.htaccess'] = 'apache_http_server',
     ['\\v^(G|g)runt\\.'] = 'gulp,javascript,nodejs',
     ['\\v^(G|g)ulpfile\\.'] = 'grunt',
     ['\\v^(md|mdown|mkd|mkdn)$'] = 'markdown',
+    ['\\v^Dockerfile$'] = 'docker',
+    ['\\v^Gemfile$'] = 'ruby',
+    ['\\v^Rakefile$'] = 'ruby',
+    ['\\v^Rakefile$'] = 'ruby',
+    ['\\v^docker-compose'] = 'docker_compose',
+    ['\\v^makefile$'] = 'make',
+    ['css'] = 'css,foundation',
+    ['help'] = 'vim',
+    ['vim'] = 'vim',
   }
 end
 function zeavim.lazy()
