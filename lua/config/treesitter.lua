b9f3589b12996ee470bf6eac1cde87ebeb9e4439
Andrew Waggoner <me@andrewjwaggoner.com>
2026-01-07 12:15:11 -0500

    update: tweaking so it works the same in WSL

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -3,7 +3,7 @@ local treesitter = {}
 function treesitter.config()
   vim.api.nvim_command('set foldmethod=expr')
   vim.api.nvim_command('set foldexpr=nvim_treesitter#foldexpr()')
-  require 'nvim-treesitter.configs'.setup {
+  require 'nvim-treesitter'.setup {
     ensure_installed = "all",
     auto_install = true,
     sync_install = false,
